4726b191076925121ea9c7827c680f844f61203e
matusoff <94809104+matusoff@users.noreply.github.com>
2022-09-10 16:43:42 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import twython
+from twython import Twython
+CONSUMER_KEY = ""    # add here your key
+CONSUMER_SECRET = "" # add here your consumer_secret
+
+twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET)
+
+# search for tweets containing the phrase "data science" # add here the search keywords
+for status in twitter.search(q='"data science"')["statuses"]:
+    user = status["user"]["screen_name"].encode('utf-8')
+    text = status["text"].encode('utf-8')
+    print(user, ":", text)
+    print()
