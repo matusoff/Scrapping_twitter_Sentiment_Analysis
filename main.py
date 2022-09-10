8805d0ec982c667d264244c818af57c8f6070ac9
matusoff <94809104+matusoff@users.noreply.github.com>
2022-09-10 16:44:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ CONSUMER_SECRET = "" # add here your consumer_secret
 
 twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET)
 
-# search for tweets containing the phrase "data science" # add here the search keywords
+# search for tweets containing the phrase "data science" 
+# add other search keywords you want to test
 for status in twitter.search(q='"data science"')["statuses"]:
     user = status["user"]["screen_name"].encode('utf-8')
     text = status["text"].encode('utf-8')
